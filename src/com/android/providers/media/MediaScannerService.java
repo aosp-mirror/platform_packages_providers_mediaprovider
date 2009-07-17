c2527694fc19e7086b025a6d5c01af75dd5d80c9
Mike Lockwood <lockwood@android.com>
2009-07-17 07:48:39 -0400

    Reduce the thread priority of the media scanner to avoid ANRs at boot time.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- src/com/android/providers/media/MediaScannerService.java
+++ src/com/android/providers/media/MediaScannerService.java
@@ -167,7 +167,10 @@ public class MediaScannerService extends Service implements Runnable
 
     public void run()
     {
-        Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        // reduce priority below other background threads to avoid interfering
+        // with other services at boot time.
+        Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND +
+                Process.THREAD_PRIORITY_LESS_FAVORABLE);
         Looper.prepare();
 
         mServiceLooper = Looper.myLooper();

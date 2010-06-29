abf8d09064cdc380d4996f5022d3a20eb4448ed1
Mike Lockwood <lockwood@android.com>
2010-06-29 16:43:41 -0400

    Start moving MTP device support into the media provider process.
    Change-Id: I9ce28ae0d92729fe7707d0fb1f4b3dfc2c77ca24
    Signed-off-by: Mike Lockwood <lockwood@android.com>

---
+++ src/com/android/providers/media/MtpService.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.providers.media;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MtpServer;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.File;
+import java.util.Locale;
+
+public class MtpService extends Service
+{
+    private static final String TAG = "MtpService";
+
+    private MtpServer mServer;
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+         Log.d(TAG, "onStart");
+
+        String storagePath = Environment.getExternalStorageDirectory().getPath();
+        String databasePath = getFilesDir().getPath() + File.pathSeparator + "mtp.db";
+        mServer = new MtpServer(storagePath, databasePath);
+        mServer.start();
+    }
+
+
+    @Override
+    public void onDestroy()
+    {
+        Log.d(TAG, "onDestroy");
+
+        mServer.stop();
+        mServer = null;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+}
+
---
+++ src/com/android/providers/media/UsbReceiver.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.providers.media;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.BroadcastReceiver;
+import android.hardware.Usb;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class UsbReceiver extends BroadcastReceiver
+{
+    private final static String TAG = "UsbReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        boolean connected = extras.getBoolean(Usb.USB_CONNECTED);
+        boolean mtpEnabled = Usb.USB_FUNCTION_ENABLED.equals(
+                extras.getString(Usb.USB_FUNCTION_MTP));
+        Log.d(TAG, "USB_CONNECTED: " + connected + " mtpEnabled: " + mtpEnabled);
+        if (connected) {
+            context.startService(new Intent(context, MtpService.class));
+        } else {
+        }
+    }
+
+}
+
+

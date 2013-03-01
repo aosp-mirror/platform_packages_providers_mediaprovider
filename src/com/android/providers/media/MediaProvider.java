aosp-mirror/platform_packages_providers_mediaprovider

commit 92013781ab1573eee3d5d75682f320fe3a6076f2

Don't create files when inserting entries
Don't automatically create files when inserting an image or video entry,
instead just generate the filename and store it in the database, and
defer creation of the file until the app calls openFile().
This could potentially break apps, but Camera and Gallery seem fine with
it, and CTS tests still pass.
b/7298303

Change-Id: Iaa311eecd6ae84f5748d8c7c3927a885f3b8c33f

src/com/android/providers/media/MediaProvider.java
@@ -2587,9 +2587,7 @@ public class MediaProvider extends ContentProvider {
             values = initialValues;
         }
 
-        if (!ensureFileExists(file)) {
-            throw new IllegalStateException("Unable to create new file: " + file);
-        }
+        // we used to create the file here, but now defer this until openFile() is called
         return values;
     }
 

commit 50c62ef29017f94bd7b83dd4650aebb23be477df

Fix album art extraction
Change 92013781ab1573eee3d5d75682f320fe3a6076f2 broke album art
scanning, because it requires the caller to ensure the parent
folder exists, which the album art scanner didn't do.

Change-Id: I4c619f3dbd75c3afa898b69059126493ea77cc07

src/com/android/providers/media/MediaProvider.java
@@ -2631,7 +2631,7 @@ public class MediaProvider extends ContentProvider {
 
     /**
      * Ensures there is a file in the _data column of values, if one isn't
-     * present a new file is created.
+     * present a new filename is generated. The file itself is not created.
      *
      * @param initialValues the values passed to insert by the caller
      * @return the new values
@@ -4653,6 +4653,9 @@ public class MediaProvider extends ContentProvider {
                 long rowId = db.insert("album_art", MediaStore.MediaColumns.DATA, values);
                 if (rowId > 0) {
                     out = ContentUris.withAppendedId(ALBUMART_URI, rowId);
+                    // ensure the parent directory exists
+                    String albumart_path = values.getAsString(MediaStore.MediaColumns.DATA);
+                    ensureFileExists(albumart_path);
                 }
             } catch (IllegalStateException ex) {
                 Log.e(TAG, "error creating album thumb file");
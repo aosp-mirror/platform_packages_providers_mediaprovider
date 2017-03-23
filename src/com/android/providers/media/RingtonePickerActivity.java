faa2ec640a0f113e45b8b661352313e28db49b86
Robin Lee <rgl@google.com>
2017-03-23 23:42:11 +0000

    Disable 'delete' context menu :(
    Change-Id: Idd00a3458c6b9777214717a6f557e76f3c0878c5

--- src/com/android/providers/media/RingtonePickerActivity.java
+++ src/com/android/providers/media/RingtonePickerActivity.java
@@ -329,6 +329,8 @@ public final class RingtonePickerActivity extends AlertActivity implements
         }
     }
 
+    // Disabled because context menus aren't Material Design :(
+    /*
     @Override
     public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
         int position = ((AdapterContextMenuInfo) menuInfo).position;
@@ -361,6 +363,7 @@ public final class RingtonePickerActivity extends AlertActivity implements
             }
         }
     }
+    */
 
     @Override
     public void onDestroy() {

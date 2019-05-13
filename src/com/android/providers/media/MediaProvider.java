48c394951c72fae4ef3683374a04ddd136165f0a
Ryan Mitchell <rtmitchell@google.com>
2019-05-13 16:40:29 -0700

    Sanitize display names to generate file names
    Allow MediaProvider to accept generating files for display names with
    forward slashes and names that start with periods.
    
    Bug: 132632971
    Test: atest MediaProviderTests
    Change-Id: Iaf0cb036c82f082a633198ef5c052d7e2923e209

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -2026,10 +2026,9 @@ public class MediaProvider extends ContentProvider {
     private static @Nullable String sanitizeDisplayName(@Nullable String name) {
         if (name == null) {
             return null;
-        } else if (name.indexOf('/') >= 0) {
-            throw new IllegalArgumentException("Directory paths not allowed: " + name);
         } else if (name.startsWith(".")) {
-            throw new IllegalArgumentException("Hidden files not allowed: " + name);
+            // The resulting file must not be hidden.
+            return FileUtils.buildValidFatFilename("_" + name);
         } else {
             return FileUtils.buildValidFatFilename(name);
         }

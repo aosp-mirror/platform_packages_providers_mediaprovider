fe1139c7129ed7e95ed4fc38e27e8c41c3933ae4
Narayan Kamath <narayan@google.com>
2019-12-30 12:45:13 +0000

    FuseDaemon: Remove bogus null checks after new.
    By default, new will throw std::bad_alloc (and bring down the process)
    if it fails, it will never return null unless "new (std::nothrow)" is
    used but that's not really advisable as there's rarely anything to be
    gained by being resilient to memory exhaustion.
    
    This is step one of several in getting rid of our explicit setting of
    errno.
    
    Test: atest FuseDaemonHostTest
    Change-Id: Ia7e2ad2088eaea979934e1cba5316c38e2111304

--- jni/FuseDaemon.cpp
+++ jni/FuseDaemon.cpp
@@ -461,16 +461,15 @@ struct node* create_node_locked(struct fuse* fuse,
     struct node* node;
 
     // Detect overflows in the inode counter. "4 billion nodes should be enough
-    // for everybody".
+    // for everybody". Crash the daemon if this happens, as this is a condition
+    // that's very unlikely to be tested thoroughly and attempting to mitigate it
+    // here is unlikely to achieve much.
     if (fuse->inode_ctr == 0) {
-        LOG(ERROR) << "No more inode numbers available";
-        return NULL;
+        LOG(FATAL) << "No more inode numbers available";
+        __builtin_unreachable();
     }
 
     node = new ::node();
-    if (!node) {
-        return NULL;
-    }
     node->name = name;
     node->nid = ptr_to_id(node);
     node->ino = fuse->inode_ctr++;
@@ -580,10 +579,6 @@ static struct node* make_node_entry(fuse_req_t req,
 
     std::lock_guard<std::mutex> lock(fuse->lock);
     node = acquire_or_create_child_locked(fuse, parent, name);
-    if (!node) {
-        errno = ENOMEM;
-        return NULL;
-    }
 
     // Manipulate attr here if needed
     e->attr_timeout = 10;
@@ -1087,18 +1082,13 @@ static void pf_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info* fi) {
         return;
     }
 
-    h = new handle(path);
-    if (!h) {
-        fuse_reply_err(req, ENOMEM);
-        return;
-    }
-
     if (fi->flags & O_DIRECT) {
         fi->flags &= ~O_DIRECT;
         fi->direct_io = true;
     }
 
     TRACE_FUSE(fuse) << "OPEN " << path;
+    h = new handle(path);
     errno = -fuse->mp->IsOpenAllowed(h->path, ctx->uid, is_requesting_write(fi->flags));
     if (errno || (h->fd = open(path.c_str(), fi->flags)) < 0) {
         delete h;
@@ -1423,11 +1413,6 @@ static void pf_opendir(fuse_req_t req,
     }
 
     h = new dirhandle();
-    if (!h) {
-        fuse_reply_err(req, ENOMEM);
-        return;
-    }
-
     errno = -fuse->mp->IsOpendirAllowed(path, ctx->uid);
     if (errno || !(h->d = opendir(path.c_str()))) {
         delete h;
@@ -1650,10 +1635,6 @@ static void pf_create(fuse_req_t req,
     child_path = parent_path + "/" + name;
 
     h = new handle(child_path);
-    if (!h) {
-        fuse_reply_err(req, ENOMEM);
-        return;
-    }
     mode = (mode & (~0777)) | 0664;
     int mp_return_code = fuse->mp->InsertFile(child_path.c_str(), ctx->uid);
     if (mp_return_code || ((h->fd = open(child_path.c_str(), fi->flags, mode)) < 0)) {

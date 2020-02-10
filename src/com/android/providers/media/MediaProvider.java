b19f11ad11a318273b46114a6c6417ad8a234181
Abhijeet Kaur <abkaur@google.com>
2020-02-10 11:35:03 +0000

    Add app compat flag to enable/disable scoped storage
    Bug: 132649864
    Test: * Sample app targetting R is not able to create test.pdf in
            DCIM (due to scoped storage restrictions)
          * `adb shell am compat disable SCOPED_STORAGE <pkg_name>` or go to
          Settings-> App Compatibility->(Select app)-> toggle SCOPED_STORAGE
          * Able to create test.pdf in DCIM (hence opt out of scoped storage)
    Change-Id: I39026d912cf9184e227c6a649f0ce0d90c440d8a

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -72,6 +72,9 @@ import android.app.DownloadManager;
 import android.app.PendingIntent;
 import android.app.RecoverableSecurityException;
 import android.app.RemoteAction;
+import android.app.compat.CompatChanges;
+import android.compat.annotation.ChangeId;
+import android.compat.annotation.EnabledAfter;
 import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.ClipDescription;
@@ -221,6 +224,17 @@ public class MediaProvider extends ContentProvider {
      * Property that indicates whether fuse is enabled.
      */
     private static final String PROP_FUSE = "persist.sys.fuse";
+
+    /**
+     * App Compatibility flag that indicates whether the app is in scoped storage or not.
+     * This flag is turned on by default for all apps targeting > Q.
+     *
+     * SCOPED_STORAGE is the Change Name and 132649864 the Change ID. Using the buganizer issue ID
+     * as the Change ID for uniqueness as documented here go/compat-framework.
+     */
+    @ChangeId
+    @EnabledAfter(targetSdkVersion=android.os.Build.VERSION_CODES.Q)
+    private static final long SCOPED_STORAGE = 132649864L;
 
     /**
      * These directory names aren't declared in Environment as final variables, and so we need to
@@ -4904,9 +4918,21 @@ public class MediaProvider extends ContentProvider {
      * </ul>
      */
     private boolean shouldBypassFuseRestrictions(boolean forWrite, String filePath) {
+        String packageName =  mCallingIdentity.get().getPackageName();
+        int uid = mCallingIdentity.get().uid;
+        boolean isScopedStorageCompatFlagEnabled;
+
+        final long token = Binder.clearCallingIdentity();
+        try {
+            isScopedStorageCompatFlagEnabled = CompatChanges.isChangeEnabled(SCOPED_STORAGE,
+                    packageName, UserHandle.getUserHandleForUid(uid));
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
+
         boolean isRequestingLegacyStorage = forWrite ? isCallingPackageLegacyWrite()
                 : isCallingPackageLegacyRead();
-        if (isRequestingLegacyStorage) {
+        if (isRequestingLegacyStorage || !isScopedStorageCompatFlagEnabled) {
             return true;
         }
 

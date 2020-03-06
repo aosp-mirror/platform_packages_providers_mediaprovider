a2fd03a909a4dfb0c61b233b4e99cc0b38acc0c7
Abhijeet Kaur <abkaur@google.com>
2020-03-06 12:43:22 +0000

    Move Scoped Storage compat flags definition to frameworks/base
    Bug: 150593967
    Test: Manual using StorageTest app
    Test: atest RestrictedPermissionsTest
    Test: atest RestrictedStoragePermissionTest
    Test: atest RestrictedStoragePermissionSharedUidTest
    Test: atest com.android.providers.media
    Change-Id: If023897c7526b7ade20dd9beaea1212f54a03ddc

--- src/com/android/providers/media/LocalCallingIdentity.java
+++ src/com/android/providers/media/LocalCallingIdentity.java
@@ -37,8 +37,6 @@ import static com.android.providers.media.util.PermissionUtils.checkPermissionWr
 import android.annotation.Nullable;
 import android.app.AppOpsManager;
 import android.app.compat.CompatChanges;
-import android.compat.annotation.ChangeId;
-import android.compat.annotation.Disabled;
 import android.content.ContentProvider;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
@@ -69,43 +67,13 @@ public class LocalCallingIdentity {
     }
 
     /**
-     * Scoped Storage is on by default. However, it is not strictly enforced and there are multiple
-     * ways to opt out of scoped storage:
-     * <ul>
-     * <li>Target Sdk < Q</li>
-     * <li>Target Sdk = Q and has `requestLegacyExternalStorage` set in AndroidManifest.xml</li>
-     * <li>Target Sdk > Q: Upgrading from an app that was opted out of scoped storage and has
-     * `preserveLegacyExternalStorage` set in AndroidManifest.xml</li>
-     * </ul>
-     * This flag is enabled for all apps by default as Scoped Storage is enabled by default.
-     * Developers can disable this flag to opt out of Scoped Storage and have legacy storage
-     * workflow.
-     *
-     * Note: {@code FORCE_ENABLE_SCOPED_STORAGE} should also be disabled for apps to opt out of
-     * scoped storage.
-     * Note: This flag is also used in android.os.Environment. Any modifications to this flag
-     * should be reflected there as well.
-     * See https://developer.android.com/training/data-storage#scoped-storage for more information.
+     * See definition in {@link android.os.Environment}
      */
-    @ChangeId
     private static final long DEFAULT_SCOPED_STORAGE = 149924527L;
 
     /**
-     * Setting this flag strictly enforces Scoped Storage regardless of:
-     * <ul>
-     * <li>The value of Target Sdk</li>
-     * <li>The value of `requestLegacyExternalStorage` in AndroidManifest.xml</li>
-     * <li>The value of `preserveLegacyExternalStorage` in AndroidManifest.xml</li>
-     * </ul>
-     *
-     * Note: {@code DEFAULT_SCOPED_STORAGE} should also be enabled for apps to be enforced into
-     * scoped storage.
-     * Note: This flag is also used in android.os.Environment. Any modifications to this flag
-     * should be reflected there as well.
-     * See https://developer.android.com/training/data-storage#scoped-storage for more information.
+     * See definition in {@link android.os.Environment}
      */
-    @ChangeId
-    @Disabled
     private static final long FORCE_ENABLE_SCOPED_STORAGE = 132649864L;
 
     public static LocalCallingIdentity fromBinder(Context context, ContentProvider provider) {

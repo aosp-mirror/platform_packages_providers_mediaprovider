c6cad05edfa289af130920d82bba314db87920f4
Ivan Chiang <chiangi@google.com>
2020-12-22 17:54:43 +0800

    Fix getVolumeName with secondary volume issue in MediaStore
    Since this method doesn't have a context. we can't find the actual
    StorageVolume for the given path, so only a vague guess is returned.
    If the path is the path of secondary volume, try to return the uuid
    from the path.
    
    Test: atest MediaStore_Audio_MediaTest#testGetContentUriForPath
    Test: atest --test-mapping packages/providers/MediaProvider
    Bug: 174288420
    Change-Id: I129c49d4fb8d68ad34444abe2dc5c435747a8161
    Merged-In: I129c49d4fb8d68ad34444abe2dc5c435747a8161

--- apex/framework/java/android/provider/MediaStore.java
+++ apex/framework/java/android/provider/MediaStore.java
@@ -76,6 +76,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -1843,6 +1844,13 @@ public final class MediaStore {
     }
 
     /**
+     * Regex that matches paths under well-known storage paths.
+     * Copied from FileUtils.java
+     */
+    private static final Pattern PATTERN_VOLUME_NAME = Pattern.compile(
+            "(?i)^/storage/([^/]+)");
+
+    /**
      * @deprecated since this method doesn't have a {@link Context}, we can't
      *             find the actual {@link StorageVolume} for the given path, so
      *             only a vague guess is returned. Callers should use
@@ -1853,9 +1861,15 @@ public final class MediaStore {
     public static @NonNull String getVolumeName(@NonNull File path) {
         // Ideally we'd find the relevant StorageVolume, but we don't have a
         // Context to obtain it from, so the best we can do is assume
-        if (path.getAbsolutePath()
-                .startsWith(Environment.getStorageDirectory().getAbsolutePath())) {
-            return MediaStore.VOLUME_EXTERNAL;
+        // Borrowed the logic from FileUtils.extractVolumeName
+        final Matcher matcher = PATTERN_VOLUME_NAME.matcher(path.getAbsolutePath());
+        if (matcher.find()) {
+            final String volumeName = matcher.group(1);
+            if (volumeName.equals("emulated")) {
+                return MediaStore.VOLUME_EXTERNAL_PRIMARY;
+            } else {
+                return volumeName.toLowerCase(Locale.ROOT);
+            }
         } else {
             return MediaStore.VOLUME_INTERNAL;
         }

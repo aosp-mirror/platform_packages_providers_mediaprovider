c78c55f2c0c276b17ea5f27cd4f5d61977691fcb
Zim <zezeozue@google.com>
2021-02-25 16:57:26 +0000

    Change handling of ANR delays
    The system will call into onAnrDelayStarted to notify the
    MediaProvider to show a progress dialog in the interim, while an app
    is about to ANR on a blocked transcoding session.
    
    See Idb9190a0e6014ce64bf1412c26f6ae03f97e922d for more details
    
    Bug: 170486601
    Test: Manual
    
    Change-Id: I890f6e36ac74c9b6d7167040b1e2ceae7f3f848d

--- src/com/android/providers/media/TranscodeHelper.java
+++ src/com/android/providers/media/TranscodeHelper.java
@@ -121,7 +121,6 @@ public class TranscodeHelper {
             "persist.sys.fuse.transcode_user_control";
     private static final String TRANSCODE_COMPAT_MANIFEST_KEY = "transcode_compat_manifest";
     private static final String TRANSCODE_COMPAT_STALE_KEY = "transcode_compat_stale";
-    private static final String TRANSCODE_ANR_DELAY_MS_KEY = "transcode_anr_delay_ms";
 
     private static final int MY_UID = android.os.Process.myUid();
 
@@ -279,16 +278,14 @@ public class TranscodeHelper {
         return new File(getTranscodeDirectory(), String.valueOf(rowId)).getAbsolutePath();
     }
 
-    public long getAnrDelayMillis(String packageName, int uid) {
-        if (!IS_TRANSCODING_SUPPORTED) {
-            Log.d(TAG, "Skipping ANR delay for older SDK version " + Build.VERSION.SDK_INT
-                    + ", code name " + Build.VERSION.CODENAME);
-            return 0;
+    public void onAnrDelayStarted(String packageName, int uid, int tid, int reason) {
+        if (!isTranscodeEnabled()) {
+            return;
         }
 
         if (uid == MY_UID) {
-            Log.w(TAG, "Skipping ANR delay for MediaProvider");
-            return 0;
+            Log.w(TAG, "Skipping ANR delay handling for MediaProvider");
+            return;
         }
 
         logVerbose("Checking transcode status during ANR of " + packageName);
@@ -300,15 +297,11 @@ public class TranscodeHelper {
 
         for (StorageTranscodingSession session: sessions) {
             if (session.isUidBlocked(uid)) {
-                int delayMs = mMediaProvider.getIntDeviceConfig(TRANSCODE_ANR_DELAY_MS_KEY, 0);
                 Log.i(TAG, "Package: " + packageName + " with uid: " + uid
-                        + " is blocked on transcoding: " + session + ". Delaying ANR by " + delayMs
-                        + "ms");
-                return delayMs;
+                        + " and tid: " + tid + " is blocked on transcoding: " + session);
+                // TODO(b/170973510): Log metrics and show UI
             }
         }
-
-        return 0;
     }
 
     // TODO(b/170974147): This should probably use a cache so we don't need to ask the

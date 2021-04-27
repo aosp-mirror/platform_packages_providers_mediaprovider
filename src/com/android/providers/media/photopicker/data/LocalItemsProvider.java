3fb37c92068e332e4c3b74b1ea6a28a65a07aa80
Abhijeet Kaur <abkaur@google.com>
2021-04-27 20:00:33 +0100

    Add implementation for LocalItemsProvider#getItems
    Add extensive unit test coverage for LocalItemsProvider class.
    
    Bug: 185905178
    Test: atest LocalItemsProviderTest
    Change-Id: I02dd07b083d02ec7332a054174bd81e77aab3bc2
    Merged-In: I02dd07b083d02ec7332a054174bd81e77aab3bc2
    (cherry picked from commit bae6d06016f046ba128645e10b41424964ade84a)

--- src/com/android/providers/media/photopicker/data/LocalItemsProvider.java
+++ src/com/android/providers/media/photopicker/data/LocalItemsProvider.java
@@ -16,10 +16,20 @@
 
 package com.android.providers.media.photopicker.data;
 
+import static com.android.providers.media.util.MimeUtils.isImageMimeType;
+import static com.android.providers.media.util.MimeUtils.isVideoMimeType;
+
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
 import android.provider.MediaStore;
+import android.provider.MediaStore.Files.FileColumns;
 import android.provider.MediaStore.MediaColumns;
 
 /**
@@ -35,25 +45,14 @@ import android.provider.MediaStore.MediaColumns;
  */
 public class LocalItemsProvider {
 
-    /**
-     * Returns a {@link Cursor} to all images/videos that are scanned by {@link MediaStore}.
-     *
-     * <p>
-     * By default the returned {@link Cursor} sorts by lastModified.
-     *
-     * @return {@link Cursor} to all images/videos on external storage that are scanned by
-     * {@link MediaStore}, or {@code null} if there are no such images/videos.
-     * The Cursor for each item would contain the following columns in their relative order:
-     * id: {@link MediaColumns#_ID} id for the image/video item,
-     * path: {@link MediaColumns#DATA} path of the image/video item,
-     * mime_type: {@link MediaColumns#MIME_TYPE} Mime type of the image/video item,
-     * is_favorite {@link MediaColumns#IS_FAVORITE} column value of the image/video item.
-     *
-     */
-     @Nullable
-     public Cursor getItems() {
-        Cursor res = null;
-        return res;
+    private static final String IMAGES_VIDEOS_WHERE_CLAUSE = "( " +
+            FileColumns.MEDIA_TYPE + " = " + FileColumns.MEDIA_TYPE_IMAGE + " OR "
+            + FileColumns.MEDIA_TYPE + " = " + FileColumns.MEDIA_TYPE_VIDEO + " )";
+
+    private final Context mContext;
+
+    public LocalItemsProvider(Context context) {
+        mContext = context;
     }
 
     /**
@@ -63,8 +62,10 @@ public class LocalItemsProvider {
      * <p>
      * By default the returned {@link Cursor} sorts by lastModified.
      *
-     * @param mimeType the type of item, only images or videos is an acceptable mimeType here.
-     *                 Any other mimeType than image/video returns all images and videos by default.
+     * @param mimeType the mime type of item, only {@code image/*} or {@code video/*} is an
+     *                 acceptable mimeType here. Any other mimeType than image/video throws error.
+     *                 {@code null} returns all images/videos that are scanned by
+     *                 {@link MediaStore}.
      *
      * @return {@link Cursor} to all images/videos on external storage that are scanned by
      * {@link MediaStore} based on {@code mimeType}, or {@code null} if there are no such
@@ -75,10 +76,58 @@ public class LocalItemsProvider {
      * mime_type: {@link MediaColumns#MIME_TYPE} Mime type of the image/video item,
      * is_favorite {@link MediaColumns#IS_FAVORITE} column value of the image/video item.
      *
+     * @throws IllegalArgumentException thrown if any mimeType other than {@code image/*} or
+     * {@code video/*} is passed.
+     *
      */
     @Nullable
-    public Cursor getItems(@NonNull String mimeType) {
-        Cursor res = null;
-        return res;
+    public Cursor getItems(@Nullable String mimeType) throws IllegalArgumentException {
+        if (mimeType == null) {
+            return queryMediaStore(null);
+        }
+        if (isMimeTypeImageVideo(mimeType)) {
+            return queryMediaStore(replaceMatchAnyChar(mimeType));
+        }
+        throw new IllegalArgumentException("LocalItemsProvider does not support the given"
+                + " mimeType: " + mimeType);
+    }
+
+    @Nullable
+    private Cursor queryMediaStore(@Nullable String mimeType) {
+        final Uri contentUri = MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL);
+
+        // TODO: This can be extracted to another class/interface as the consumer of this getItems
+        // needs to be aware of what the Cursor has returned and how to access it. Instead of
+        // hardcoding projection values in both the layers.
+        final String[] projection = new String[] {
+                MediaColumns._ID, MediaColumns.DATA, MediaColumns.MIME_TYPE,
+                MediaColumns.IS_FAVORITE };
+
+        String selection = IMAGES_VIDEOS_WHERE_CLAUSE;
+        String[] selectionArgs = null;
+
+        if (mimeType != null) {
+            selection += " AND " + MediaColumns.MIME_TYPE + " LIKE ? ";
+            selectionArgs = new String[] {mimeType};
+        }
+
+        try (ContentProviderClient client = mContext.getContentResolver()
+                .acquireUnstableContentProviderClient(MediaStore.AUTHORITY)) {
+            Bundle extras = new Bundle();
+            extras.putString(ContentResolver.QUERY_ARG_SQL_SELECTION, selection);
+            extras.putStringArray(ContentResolver.QUERY_ARG_SQL_SELECTION_ARGS, selectionArgs);
+            return client.query(contentUri, projection, extras, null);
+        } catch (RemoteException ignored) {
+            // Do nothing, return null.
+        }
+        return null;
+    }
+
+    private boolean isMimeTypeImageVideo(@NonNull String mimeType) {
+        return isImageMimeType(mimeType) || isVideoMimeType(mimeType);
+    }
+
+    private String replaceMatchAnyChar(@NonNull String mimeType) {
+        return mimeType.replace('*', '%');
     }
 }
--- tests/src/com/android/providers/media/photopicker/LocalItemsProviderTest.java
+++ tests/src/com/android/providers/media/photopicker/LocalItemsProviderTest.java
@@ -16,45 +16,413 @@
 
 package com.android.providers.media.photopicker;
 
+import static android.provider.MediaStore.VOLUME_EXTERNAL;
+
+import static com.android.providers.media.util.MimeUtils.isImageMimeType;
+import static com.android.providers.media.util.MimeUtils.isVideoMimeType;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.fail;
+
+import android.content.Context;
 import android.database.Cursor;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.test.InstrumentationRegistry;
 
 import com.android.providers.media.photopicker.data.LocalItemsProvider;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+
 public class LocalItemsProviderTest {
+
+    /**
+     * To help avoid flaky tests, give ourselves a unique nonce to be used for
+     * all filesystem paths, so that we don't risk conflicting with previous
+     * test runs.
+     */
+    private static final String NONCE = String.valueOf(System.nanoTime());
+    private static final String TAG = "LocalItemsProviderTest";
+    private static final String VIDEO_FILE_NAME = TAG + "_file_" + NONCE + ".mp4";
+    private static final String IMAGE_FILE_NAME = TAG + "_file_" + NONCE + ".jpg";
+    private static final String HIDDEN_DIR_NAME = TAG + "_hidden_dir_" + NONCE;
+
+    private Context mContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mContext = InstrumentationRegistry.getTargetContext();
+        // Wait for MediaStore to be Idle to reduce flakes caused by database updates
+        MediaStore.waitForIdle(mContext.getContentResolver());
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} to return all images and videos.
+     *
+     * @throws Exception
+     */
     @Test
     public void testGetItems() throws Exception {
-        LocalItemsProvider localItemsProvider = new LocalItemsProvider();
-        Cursor res = localItemsProvider.getItems();
-        // TODO: Add assertions when the implementation is added
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems(null);
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file to test {@link LocalItemsProvider#getItems(String)}.
+        // Both files should be returned.
+        File imageFile = assertCreateNewImage();
+        File videoFile = assertCreateNewVideo();
+        try {
+            res = localItemsProvider.getItems(null);
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems + 2);
+
+            assertThatOnlyImagesVideos(res);
+            assertThatAllImagesVideos(res.getCount());
+        } finally {
+            imageFile.delete();
+            videoFile.delete();
+        }
     }
 
+    /**
+     * Tests {@link {@link LocalItemsProvider#getItems(String)}} does not return hidden
+     * images/videos.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testGetItemsAll() throws Exception {
-        LocalItemsProvider localItemsProvider = new LocalItemsProvider();
-        Cursor res = localItemsProvider.getItems("*/*");
-        // TODO: Add assertions when the implementation is added
+    public void testGetItems_nonMedia() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems(null);
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file in a hidden dir to test
+        // {@link LocalItemsProvider#getItems(String)}. Both should not be returned.
+        File hiddenDir = createHiddenDir();
+        File imageFileHidden = assertCreateNewImage(hiddenDir);
+        File videoFileHidden = assertCreateNewVideo(hiddenDir);
+        try {
+            res = localItemsProvider.getItems(null);
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems);
+        } finally {
+            imageFileHidden.delete();
+            videoFileHidden.delete();
+            hiddenDir.delete();
+        }
     }
 
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} to return all images and videos based on
+     * the mimeType. Image mimeType should only return images.
+     *
+     * @throws Exception
+     */
     @Test
     public void testGetItemsImages() throws Exception {
-        LocalItemsProvider localItemsProvider = new LocalItemsProvider();
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
         Cursor res = localItemsProvider.getItems("image/*");
-        // TODO: Add assertions when the implementation is added
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file to test {@link LocalItemsProvider#getItems(String)}.
+        // Only 1 should be returned.
+        File imageFile = assertCreateNewImage();
+        File videoFile = assertCreateNewVideo();
+        try {
+            res = localItemsProvider.getItems("image/*");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems + 1);
+
+            assertThatOnlyImages(res);
+            assertThatAllImages(res.getCount());
+        } finally {
+            imageFile.delete();
+            videoFile.delete();
+        }
     }
 
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} to return all images and videos based on
+     * the mimeType. Image mimeType should only return images.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetItemsImages_png() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems("image/png");
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create a jpg file image. Tests negative use case, this should not be returned below.
+        File imageFile = assertCreateNewImage();
+        try {
+            res = localItemsProvider.getItems("image/png");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems);
+        } finally {
+            imageFile.delete();
+        }
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} does not return hidden images/videos.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetItemsImages_nonMedia() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems("image/*");
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file in a hidden dir to test
+        // {@link LocalItemsProvider#getItems(String)}. Both should not be returned.
+        File hiddenDir = createHiddenDir();
+        File imageFileHidden = assertCreateNewImage(hiddenDir);
+        File videoFileHidden = assertCreateNewVideo(hiddenDir);
+        try {
+            res = localItemsProvider.getItems("image/*");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems);
+        } finally {
+            imageFileHidden.delete();
+            videoFileHidden.delete();
+            hiddenDir.delete();
+        }
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} to return all images and videos based on
+     * the mimeType. Video mimeType should only return videos.
+     *
+     * @throws Exception
+     */
     @Test
     public void testGetItemsVideos() throws Exception {
-        LocalItemsProvider localItemsProvider = new LocalItemsProvider();
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
         Cursor res = localItemsProvider.getItems("video/*");
-        // TODO: Add assertions when the implementation is added
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file to test {@link LocalItemsProvider#getItems(String)}.
+        // Only 1 should be returned.
+        File imageFile = assertCreateNewImage();
+        File videoFile = assertCreateNewVideo();
+        try {
+            res = localItemsProvider.getItems("video/*");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems + 1);
+
+            assertThatOnlyVideos(res);
+            assertThatAllVideos(res.getCount());
+        } finally {
+            imageFile.delete();
+            videoFile.delete();
+        }
     }
 
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} to return all images and videos based on
+     * the mimeType. Image mimeType should only return images.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetItemsVideos_mp4() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems("video/mp4");
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create a mp4 video file. Tests positive use case, this should be returned below.
+        File videoFile = assertCreateNewVideo();
+        try {
+            res = localItemsProvider.getItems("video/mp4");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems + 1);
+        } finally {
+            videoFile.delete();
+        }
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} does not return hidden images/videos.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetItemsVideos_nonMedia() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        Cursor res = localItemsProvider.getItems("video/*");
+        assertThat(res).isNotNull();
+        final int initialCountOfItems = res.getCount();
+
+        // Create 1 image and 1 video file in a hidden dir to test the API.
+        // Both should not be returned.
+        File hiddenDir = createHiddenDir();
+        File imageFileHidden = assertCreateNewImage(hiddenDir);
+        File videoFileHidden = assertCreateNewVideo(hiddenDir);
+        try {
+            res = localItemsProvider.getItems("video/*");
+            assertThat(res).isNotNull();
+            final int laterCountOfItems = res.getCount();
+
+            assertThat(laterCountOfItems).isEqualTo(initialCountOfItems);
+        } finally {
+            imageFileHidden.delete();
+            videoFileHidden.delete();
+            hiddenDir.delete();
+        }
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} throws error for invalid param for
+     * mimeType.
+     *
+     * @throws Exception
+     */
     @Test
     public void testGetItemsInvalidParam() throws Exception {
-        LocalItemsProvider localItemsProvider = new LocalItemsProvider();
-        Cursor res = localItemsProvider.getItems("audio/*");
-        // TODO: Add assertions when the implementation is added
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        try {
+            localItemsProvider.getItems("audio/*");
+            fail("Expected IllegalArgumentException for audio mimeType");
+        } catch (IllegalArgumentException expected) {
+            // Expected flow
+        }
+    }
+
+    /**
+     * Tests {@link LocalItemsProvider#getItems(String)} throws error for invalid param for
+     * mimeType.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetItemsAllMimeType() throws Exception {
+        LocalItemsProvider localItemsProvider = new LocalItemsProvider(mContext);
+        try {
+            localItemsProvider.getItems("*/*");
+            fail("Expected IllegalArgumentException for audio mimeType");
+        } catch (IllegalArgumentException expected) {
+            // Expected flow
+        }
+    }
+
+    private void assertThatOnlyImagesVideos(Cursor c) throws Exception {
+        while (c.moveToNext()) {
+            String mimeType = c.getString(2);
+            assertThat(isImageMimeType(mimeType) || isVideoMimeType(mimeType)).isTrue();
+        }
+    }
+
+    private void assertThatOnlyImages(Cursor c) throws Exception {
+        while (c.moveToNext()) {
+            String mimeType = c.getString(2);
+            assertThat(isImageMimeType(mimeType)).isTrue();
+        }
+    }
+
+    private void assertThatOnlyVideos(Cursor c) throws Exception {
+        while (c.moveToNext()) {
+            String mimeType = c.getString(2);
+            assertThat(isVideoMimeType(mimeType)).isTrue();
+        }
+    }
+
+    private void assertThatAllImagesVideos(int count) {
+        int countOfImages = getCountOfMediaStoreImages();
+        int countOfVideos = getCountOfMediaStoreVideos();
+        assertThat(count).isEqualTo(countOfImages + countOfVideos);
+    }
+
+    private void assertThatAllImages(int count) {
+        int countOfImages = getCountOfMediaStoreImages();
+        assertThat(count).isEqualTo(countOfImages);
+    }
+
+    private void assertThatAllVideos(int count) {
+        int countOfVideos = getCountOfMediaStoreVideos();
+        assertThat(count).isEqualTo(countOfVideos);
+    }
+
+    private int getCountOfMediaStoreImages() {
+        try (Cursor c = mContext.getContentResolver().query(
+                MediaStore.Images.Media.getContentUri(VOLUME_EXTERNAL), null, null, null)) {
+            assertThat(c.moveToFirst()).isTrue();
+            return c.getCount();
+        }
+    }
+
+    private int getCountOfMediaStoreVideos() {
+        try (Cursor c = mContext.getContentResolver().query(
+                MediaStore.Video.Media.getContentUri(VOLUME_EXTERNAL), null, null, null)) {
+            assertThat(c.moveToFirst()).isTrue();
+            return c.getCount();
+        }
+    }
+
+    private File assertCreateNewVideo(File dir) throws Exception {
+        return assertCreateNewFile(dir, VIDEO_FILE_NAME);
+    }
+
+    private File assertCreateNewImage(File dir) throws Exception {
+        return assertCreateNewFile(dir, IMAGE_FILE_NAME);
+    }
+
+    private File assertCreateNewVideo() throws Exception {
+        return assertCreateNewFile(getDownloadsDir(), VIDEO_FILE_NAME);
+    }
+
+    private File assertCreateNewImage() throws Exception {
+        return assertCreateNewFile(getDownloadsDir(), IMAGE_FILE_NAME);
+    }
+
+    private File assertCreateNewFile(File dir, String fileName) throws Exception {
+        final File file = new File(dir, fileName);
+        assertThat(file.createNewFile()).isTrue();
+        MediaStore.scanFile(mContext.getContentResolver(), file);
+        return file;
+    }
+
+    private File getDownloadsDir() {
+        return new File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOWNLOADS);
+    }
+
+    private File createHiddenDir() throws Exception {
+        File parentDir = new File(Environment.getExternalStorageDirectory(),
+                Environment.DIRECTORY_DOWNLOADS);
+        File dir = new File(parentDir, HIDDEN_DIR_NAME);
+        dir.mkdirs();
+        File nomedia = new File(dir, ".nomedia");
+        nomedia.createNewFile();
+        MediaStore.scanFile(mContext.getContentResolver(), nomedia);
+
+        return dir;
     }
 }

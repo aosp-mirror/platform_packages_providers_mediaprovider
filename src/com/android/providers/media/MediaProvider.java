b13a86b7f74192a711d8a2934adc0eb87c85232a
Zim <zezeozue@google.com>
2021-07-23 17:23:53 +0100

    Add a local PhotoPickerProvider
    The PhotoPickerProvider implements the CloudMediaProvider interface to
    sync local media into the picker db. The provider implementation is
    quite straightforward since the details of querying the external db
    are abstracted away in the ExternalDbFacadeForPicker.
    
    Bug: 190713331
    Test: Manual
    Change-Id: I48fe0472a050bdd1920fca6f03b18bbe2aacbb5d

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -9757,6 +9757,10 @@ public class MediaProvider extends ContentProvider {
         return builder.build();
     }
 
+    public DatabaseHelper getExternalDatabaseHelper() {
+        return mExternalDatabase;
+    }
+
     private boolean isCallingPackageSystemGallery() {
         return mCallingIdentity.get().hasPermission(PERMISSION_IS_SYSTEM_GALLERY);
     }

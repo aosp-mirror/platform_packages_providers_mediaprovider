e5375cb6e5350563b041288cfd8a0fe1f2c51b49
Ivan Chiang <chiangi@google.com>
2021-08-16 15:13:32 +0800

    Handle content uris without id in checkUriPermission
    Any app that has access to exactly 1 file to a collection (E.g.
    Audio, Downloads, Files, Images or Videos) can gain access (read and
    write) to all files in that collection without asking for extra
    permissions. The app can do this by requesting the uri grant with
    FLAG_GRANT_PREFIX_URI_PERMISSION on content uri to the collection.
    Don't allow the apps grant FLAG_GRANT_PREFIX_URI_PERMISSION in this
    case.
    
    If the app is granted the read UriPerimssion on the uri without id,
    don't allow the app to get global access. Then, the app gets the same
    query result of the uri without read UriPermission.
    
    Test: atest ExternalStorageHostTest
    Test: atest --test-mapping packages/providers/MediaProvider
    Bug: 194539422
    Bug: 197302116
    Change-Id: Id6349b7b49cf682c6703333bc96f1b7046b7700e
    Merged-In: Id6349b7b49cf682c6703333bc96f1b7046b7700e
    (cherry picked from commit ef2abc716b7a9d53677cd6df9b6938c0e3138e79)
    Merged-In:Id6349b7b49cf682c6703333bc96f1b7046b7700e

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -2622,15 +2622,41 @@ public class MediaProvider extends ContentProvider {
             try (Cursor c = qb.query(helper,
                     new String[] { BaseColumns._ID }, null, null, null, null, null, null, null)) {
                 if (c.getCount() == 1) {
-                    return PackageManager.PERMISSION_GRANTED;
+                    c.moveToFirst();
+                    final long cursorId = c.getLong(0);
+
+                    long uriId = -1;
+                    try {
+                        uriId = ContentUris.parseId(uri);
+                    } catch (NumberFormatException ignored) {
+                        // if the id is not a number, the uri doesn't have a valid ID at the end of
+                        // the uri, (i.e., uri is uri of the table not of the item/row)
+                    }
+
+                    if (uriId != -1 && cursorId == uriId) {
+                        return PackageManager.PERMISSION_GRANTED;
+                    }
                 }
             }
 
-            try {
-                if (ContentUris.parseId(uri) != -1) {
+            // For the uri with id cases, if it isn't returned in above query section, the result
+            // isn't as expected. Don't grant the permission.
+            switch (table) {
+                case AUDIO_MEDIA_ID:
+                case IMAGES_MEDIA_ID:
+                case VIDEO_MEDIA_ID:
+                case DOWNLOADS_ID:
+                case FILES_ID:
+                case AUDIO_MEDIA_ID_GENRES_ID:
+                case AUDIO_GENRES_ID:
+                case AUDIO_PLAYLISTS_ID:
+                case AUDIO_PLAYLISTS_ID_MEMBERS_ID:
+                case AUDIO_ARTISTS_ID:
+                case AUDIO_ALBUMS_ID:
                     return PackageManager.PERMISSION_DENIED;
-                }
-            } catch (NumberFormatException ignored) { }
+                default:
+                    // continue below
+            }
 
             // If the uri is a valid content uri and doesn't have a valid ID at the end of the uri,
             // (i.e., uri is uri of the table not of the item/row), and app doesn't request prefix
@@ -2640,9 +2666,6 @@ public class MediaProvider extends ContentProvider {
             if ((modeFlags & Intent.FLAG_GRANT_PREFIX_URI_PERMISSION) == 0) {
                 return PackageManager.PERMISSION_GRANTED;
             }
-
-            // For prefix grant on the uri with content uri without id, we don't allow apps to
-            // grant access as they might end up granting access to all files.
         } finally {
             restoreLocalCallingIdentity(token);
         }
@@ -8822,6 +8845,26 @@ public class MediaProvider extends ContentProvider {
                 if (mCallingIdentity.get().isOwned(id)) {
                     return true;
                 }
+                break;
+            default:
+                // continue below
+        }
+
+        // Check whether the uri is a specific table or not. Don't allow the global access to these
+        // table uris
+        switch (table) {
+            case AUDIO_MEDIA:
+            case IMAGES_MEDIA:
+            case VIDEO_MEDIA:
+            case DOWNLOADS:
+            case FILES:
+            case AUDIO_ALBUMS:
+            case AUDIO_ARTISTS:
+            case AUDIO_GENRES:
+            case AUDIO_PLAYLISTS:
+                return false;
+            default:
+                // continue below
         }
 
         // Outstanding grant means they get access

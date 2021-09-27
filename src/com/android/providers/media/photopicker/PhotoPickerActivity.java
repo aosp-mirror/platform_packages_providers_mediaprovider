9218cb2284e52cefc082ad460bbb94d03a71a68a
Abhijeet Kaur <abkaur@google.com>
2021-09-27 16:22:33 +0100

    Modify Photo Picker return values
    * PhotoPicker always returns picker uris. This enables cross-profile
    content sharing via Photo Picker.
    * Modify to return clipData for multi-select and single-select mode
    
    Bug: 190727056
    Test: atest PickerResultTest
    Change-Id: I304ef683b0ad71b30be8f6f8d1f7051fec97b706

--- src/com/android/providers/media/photopicker/PhotoPickerActivity.java
+++ src/com/android/providers/media/photopicker/PhotoPickerActivity.java
@@ -341,16 +341,9 @@ public class PhotoPickerActivity extends AppCompatActivity {
     public void setResultAndFinishSelf() {
         final List<Item> selectedItemList = new ArrayList<>(
                 mPickerViewModel.getSelectedItems().getValue().values());
-        // "persist.sys.photopicker.usepickeruri" property is used to indicate if picker uris should
-        // be returned for all intent actions.
-        // TODO(b/168001592): Remove this system property when intent-filter for ACTION_GET_CONTENT
-        // is removed or when we don't have to send redactedUris any more.
-        final boolean usePickerUriByDefault =
-                SystemProperties.getBoolean("persist.sys.photopicker.usepickeruri", false);
-        final boolean shouldReturnPickerUris = usePickerUriByDefault ||
-                MediaStore.ACTION_PICK_IMAGES.equals(getIntent().getAction());
-        setResult(Activity.RESULT_OK, getPickerResponseIntent(this, selectedItemList,
-                shouldReturnPickerUris));
+
+        setResult(Activity.RESULT_OK, getPickerResponseIntent(mPickerViewModel.canSelectMultiple(),
+                selectedItemList));
         finish();
     }
 

1fb4d347768512ee5cf0f08e505fb0adb1209e9c
Ivan Chiang <chiangi@google.com>
2021-10-08 16:04:41 +0800

    Hide the column in query result by @ExportedSince value
    - Add the @ExportedSince Build.VERSION_CODES.S on is_recording
    - If the column is added @ExportedSince by X version, if the os
      version is less than X, the query result won't include the column.
    
    Test: atest MediaProviderTest
    Test: atest DatabaseHelperTest
    Bug: 202040814
    Change-Id: Ib019feb0ef715ac1b828ae5c39140bcfb80cf51e

--- tests/src/com/android/providers/media/DatabaseHelperTest.java
+++ tests/src/com/android/providers/media/DatabaseHelperTest.java
@@ -42,6 +42,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.UserHandle;
 import android.provider.Column;
+import android.provider.ExportedSince;
 import android.provider.MediaStore.Audio.AudioColumns;
 import android.provider.MediaStore.Audio;
 import android.provider.MediaStore.Files.FileColumns;
@@ -635,8 +636,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperO extends DatabaseHelper {
         public DatabaseHelperO(Context context, String name) {
-            super(context, name, DatabaseHelper.VERSION_O,
-                    false, false, Column.class, null, null, null, null);
+            super(context, name, DatabaseHelper.VERSION_O, false, false, Column.class,
+                    ExportedSince.class, null, null, null, null);
         }
 
         @Override
@@ -647,8 +648,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperP extends DatabaseHelper {
         public DatabaseHelperP(Context context, String name) {
-            super(context, name, DatabaseHelper.VERSION_P,
-                    false, false, Column.class, null, null, null, null);
+            super(context, name, DatabaseHelper.VERSION_P, false, false, Column.class,
+                    ExportedSince.class, null, null, null, null);
         }
 
         @Override
@@ -659,8 +660,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperQ extends DatabaseHelper {
         public DatabaseHelperQ(Context context, String name) {
-            super(context, name, DatabaseHelper.VERSION_Q,
-                    false, false, Column.class, null, null, null, null);
+            super(context, name, DatabaseHelper.VERSION_Q, false, false, Column.class,
+                    ExportedSince.class, null, null, null, null);
         }
 
         @Override
@@ -671,9 +672,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperR extends DatabaseHelper {
         public DatabaseHelperR(Context context, String name) {
-            super(context, name, DatabaseHelper.VERSION_R,
-                    false, false, Column.class, null, null,
-                    MediaProvider.MIGRATION_LISTENER, null);
+            super(context, name, DatabaseHelper.VERSION_R, false, false, Column.class,
+                    ExportedSince.class, null, null, MediaProvider.MIGRATION_LISTENER, null);
         }
 
         @Override
@@ -684,9 +684,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperS extends DatabaseHelper {
         public DatabaseHelperS(Context context, String name) {
-            super(context, name, VERSION_S,
-                    false, false, Column.class, null, null,
-                    MediaProvider.MIGRATION_LISTENER, null);
+            super(context, name, VERSION_S, false, false, Column.class, ExportedSince.class, null,
+                    null, MediaProvider.MIGRATION_LISTENER, null);
         }
 
 
@@ -698,9 +697,8 @@ public class DatabaseHelperTest {
 
     private static class DatabaseHelperT extends DatabaseHelper {
         public DatabaseHelperT(Context context, String name) {
-            super(context, name, DatabaseHelper.VERSION_T,
-                    false, false, Column.class, null, null,
-                    MediaProvider.MIGRATION_LISTENER, null);
+            super(context, name, DatabaseHelper.VERSION_T, false, false, Column.class,
+                    ExportedSince.class, null, null, MediaProvider.MIGRATION_LISTENER, null);
         }
     }
 

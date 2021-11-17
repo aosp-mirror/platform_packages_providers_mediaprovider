3bc157706904f0c8929ef884f482bc361cca6ff8
Abhijeet Kaur <abkaur@google.com>
2021-11-17 08:40:34 +0000

    Restrict legacy apps to insert files to other private app dirs
    We allow legacy apps to insert/update content values such that they can
    add private files to MediaStore database. Restrict such apps to
    insert/update content in other app's private directories. Add extensive
    logic to allow apps with special access (installers, access_mtp,
    downloadsprovider) to have same level of access to other app's private
    directories.
    
    Retrict System Gallery apps and apps with MANAGE_EXTERNAL_STORAGE
    permission to insert private path files.
    
    Bug: 201667614
    Test: atest FileUtilsTest
    Test: atest CtsScopedStorageHostTest
    Test: atest LegacyStorageTest:LegacyStorageHostTest
    Change-Id: I52481b24b137790d49845f1a4b00978c63ed183b
    Merged-In: I52481b24b137790d49845f1a4b00978c63ed183b
    (cherry picked from commit 220971d4e9dddb91d8073c0c295fe0fff057249d)
    Merged-In:I52481b24b137790d49845f1a4b00978c63ed183b

--- src/com/android/providers/media/LocalCallingIdentity.java
+++ src/com/android/providers/media/LocalCallingIdentity.java
@@ -21,8 +21,11 @@ import static android.app.AppOpsManager.MODE_ALLOWED;
 import static android.app.AppOpsManager.permissionToOp;
 import static android.content.pm.PackageManager.PERMISSION_DENIED;
 
+import static com.android.providers.media.util.PermissionUtils.checkAppOpRequestInstallPackagesForSharedUid;
 import static com.android.providers.media.util.PermissionUtils.checkIsLegacyStorageGranted;
+import static com.android.providers.media.util.PermissionUtils.checkPermissionAccessMtp;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionDelegator;
+import static com.android.providers.media.util.PermissionUtils.checkPermissionInstallPackages;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionManager;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionReadAudio;
 import static com.android.providers.media.util.PermissionUtils.checkPermissionReadImages;
@@ -213,6 +216,18 @@ public class LocalCallingIdentity {
     public static final int PERMISSION_WRITE_VIDEO = 1 << 20;
     public static final int PERMISSION_WRITE_IMAGES = 1 << 21;
 
+    /**
+     * Explicitly checks **only** for INSTALL_PACKAGES runtime permission.
+     */
+    public static final int PERMISSION_INSTALL_PACKAGES = 1 << 23;
+    public static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 1 << 24;
+
+    /**
+     * Checks if REQUEST_INSTALL_PACKAGES app-op is allowed for any package sharing this UID.
+     */
+    public static final int APPOP_REQUEST_INSTALL_PACKAGES_FOR_SHARED_UID = 1 << 25;
+    public static final int PERMISSION_ACCESS_MTP = 1 << 26;
+
     private int hasPermission;
     private int hasPermissionResolved;
 
@@ -253,6 +268,10 @@ public class LocalCallingIdentity {
             case PERMISSION_IS_LEGACY_WRITE:
                 return isLegacyWriteInternal();
 
+            case PERMISSION_WRITE_EXTERNAL_STORAGE:
+                return checkPermissionWriteStorage(
+                        context, pid, uid, getPackageName(), attributionTag);
+
             case PERMISSION_READ_AUDIO:
                 return checkPermissionReadAudio(
                         context, pid, uid, getPackageName(), attributionTag);
@@ -270,6 +289,15 @@ public class LocalCallingIdentity {
                         context, pid, uid, getPackageName(), attributionTag);
             case PERMISSION_WRITE_IMAGES:
                 return checkPermissionWriteImages(
+                        context, pid, uid, getPackageName(), attributionTag);
+            case PERMISSION_INSTALL_PACKAGES:
+                return checkPermissionInstallPackages(
+                        context, pid, uid, getPackageName(), attributionTag);
+            case APPOP_REQUEST_INSTALL_PACKAGES_FOR_SHARED_UID:
+                return checkAppOpRequestInstallPackagesForSharedUid(
+                        context, uid, getSharedPackageNames(), attributionTag);
+            case PERMISSION_ACCESS_MTP:
+                return checkPermissionAccessMtp(
                         context, pid, uid, getPackageName(), attributionTag);
             default:
                 return false;

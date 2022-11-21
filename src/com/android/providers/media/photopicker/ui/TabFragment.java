0d2c08b800687f61e0039336dbc5acfead956266
Krishang Garodia <krishang@google.com>
2022-11-21 17:36:59 +0000

    Add picker banner text and buttons dynamically
    Adding the picker banner text and buttons dynamically (without the click
    listeners for the buttons and the banner as hidden) to act as an outline
    for further banner changes.
    
    Test: Functional
    Bug: 195009152
    Change-Id: Ic2eb29512509d9daf7baf4ac3607dee0de868df8

--- src/com/android/providers/media/photopicker/ui/TabFragment.java
+++ src/com/android/providers/media/photopicker/ui/TabFragment.java
@@ -55,6 +55,7 @@ import com.android.providers.media.R;
 import com.android.providers.media.photopicker.PhotoPickerActivity;
 import com.android.providers.media.photopicker.data.Selection;
 import com.android.providers.media.photopicker.data.UserIdManager;
+import com.android.providers.media.photopicker.viewmodel.BannerViewModel;
 import com.android.providers.media.photopicker.viewmodel.PickerViewModel;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
@@ -68,6 +69,7 @@ import java.util.Locale;
 public abstract class TabFragment extends Fragment {
 
     protected PickerViewModel mPickerViewModel;
+    protected BannerViewModel mBannerViewModel;
     protected Selection mSelection;
     protected ImageLoader mImageLoader;
     protected AutoFitRecyclerView mRecyclerView;
@@ -117,7 +119,9 @@ public abstract class TabFragment extends Fragment {
         mImageLoader = new ImageLoader(context);
         mRecyclerView = view.findViewById(R.id.picker_tab_recyclerview);
         mRecyclerView.setHasFixedSize(true);
-        mPickerViewModel = new ViewModelProvider(requireActivity()).get(PickerViewModel.class);
+        final ViewModelProvider viewModelProvider = new ViewModelProvider(requireActivity());
+        mPickerViewModel = viewModelProvider.get(PickerViewModel.class);
+        mBannerViewModel = viewModelProvider.get(BannerViewModel.class);
         mSelection = mPickerViewModel.getSelection();
         mRecyclerViewBottomPadding = getResources().getDimensionPixelSize(
                 R.dimen.picker_recycler_view_bottom_padding);
@@ -418,10 +422,6 @@ public abstract class TabFragment extends Fragment {
                         : context.getString(R.string.picker_add_button_multi_select);
 
         return TextUtils.expandTemplate(template, sizeString).toString();
-    }
-
-    protected final void observeAndUpdateBannerVisibility(@NonNull TabAdapter adapter) {
-        mPickerViewModel.getBannerVisibilityLiveData().observe(this, adapter::setShowBanner);
     }
 
     protected final PhotoPickerActivity getPickerActivity() {

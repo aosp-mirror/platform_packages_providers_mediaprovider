1426f04cd2b4940f9308432d37cc4ef79a7d8340
Abhijeet Kaur <abkaur@google.com>
2022-11-23 08:47:27 +0000

    Canonicalise path before extracting relative path
    This helps us make accurate access checks on the given path.
    
    Bug: 228833816
    Bug: 228450832
    Test: atest FileUtilsTest
    Test: atest LegacyStorageHostTest
    Change-Id: Id620644ffdfe20e9281773e2e23851c56732dd11
    Merged-In: Id620644ffdfe20e9281773e2e23851c56732dd11
    (cherry picked from commit 93f5186e4b4a044e00a168c55e05fd3835033221)

--- src/com/android/providers/media/util/FileUtils.java
+++ src/com/android/providers/media/util/FileUtils.java
@@ -1110,7 +1110,9 @@ public class FileUtils {
     }
 
     public static @Nullable String extractRelativePath(@Nullable String data) {
+        data = getCanonicalPath(data);
         if (data == null) return null;
+
         final Matcher matcher = PATTERN_RELATIVE_PATH.matcher(data);
         if (matcher.find()) {
             final int lastSlash = data.lastIndexOf('/');
@@ -1757,5 +1759,17 @@ public class FileUtils {
 
     public static File fromFuseFile(File file) {
         return new File(file.getPath().replaceFirst(FUSE_FS_PREFIX, LOWER_FS_PREFIX));
+    }
+
+    @Nullable
+    private static String getCanonicalPath(@Nullable String path) {
+        if (path == null) return null;
+
+        try {
+            return new File(path).getCanonicalPath();
+        } catch (IOException e) {
+            Log.d(TAG, "Unable to get canonical path from invalid data path: " + path, e);
+            return null;
+        }
     }
 }

9c41dd3a82774726acc85cc8821246d57edfda7a
Dipankar Bhardwaj <dipankarb@google.com>
2024-01-17 18:38:36 +0000

    Remove leveldb connections on detachVolume
    Bug: 319802404
    Test: android.os.storage.cts.StorageManagerTest#testCallback
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0755f9d105f88003655f0ef8778c6e5a9f3f0757)
    Merged-In: I02ddd34e690d9755e045840f3ccbdedc21e97819
    Change-Id: I02ddd34e690d9755e045840f3ccbdedc21e97819

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -11099,6 +11099,7 @@ public class MediaProvider extends ContentProvider {
                     "Deleting the internal volume is not allowed");
         }
 
+        mDatabaseBackupAndRecovery.onDetachVolume(volume);
         // Signal any scanning to shut down
         mMediaScanner.onDetachVolume(volume);
 
